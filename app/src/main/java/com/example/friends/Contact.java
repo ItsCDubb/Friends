9a941ce476f852d6c57a9ee0c97e5f9c9eb9cfe8
ItsCDubb <chris.warren.2019@gmail.com>
2019-06-14 10:01:15 -0500

    version 0.75

--- app/src/main/java/com/example/friends/Contact.java
+++ app/src/main/java/com/example/friends/Contact.java
@@ -2,10 +2,23 @@ package com.example.friends;
 
 public class Contact {
 
-    private String name;
-    private String phoneNumber;
+    private String name, phoneNumber;
+
+    public Contact(String name, phoneNumber) {
+        this.name        = name;
+        this.phoneNumber = phoneNumber;
+    }
 
     public String getName() {
+
         return name;
+
     }
+
+    public String getPhoneNumber() {
+
+        return phoneNumber;
+    }
+
+
 }
--- app/src/main/java/com/example/friends/ContactsManager.java
+++ app/src/main/java/com/example/friends/ContactsManager.java
@@ -1,4 +1,14 @@
 package com.example.friends;
 
+import java.util.ArrayList;
+
 public class ContactsManager {
+
+    private ArrayList<Contact> friend = new ArrayList<Contact>();
+
+    public void createContact (String name, String phoneNumber) {
+
+        Contact contact = new Contact(name, phoneNumber);
+        friend.add(contact);
+    }
 }
